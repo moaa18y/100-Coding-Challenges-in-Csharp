391b24c61aec0616e0801458cd09a93756ea3381
Moamen Abu Al-Saud <moaa18y@gmail.com>
2026-04-22 22:32:44 +0200

    Challenge 4 Vowel Counter

---
+++ Challenge 4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Challenge_4
+{
+    internal class Program
+    {
+        //Vowel Counter: Count (a, e, i, o, u) in a string.
+        //Ex: "Hello" => 2
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string:");
+            string input = Console.ReadLine();
+            int vowelCount = CountVowels(input);
+            Console.WriteLine($"Number of vowels: {vowelCount}");
+        }
+        static int CountVowels(string input)
+        {
+            int count = 0;
+            string vowels = "aeiouAEIOU";
+            foreach (char c in input)
+            {
+                if (vowels.Contains(c))
+                    count++;
+            }
+            return count;
+        }
+    }
+}

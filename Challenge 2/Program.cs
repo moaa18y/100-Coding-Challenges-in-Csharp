6d6622a384a8618900f2ff18411d77ac3c2427c8
Moamen Abu Al-Saud <moaa18y@gmail.com>
2026-04-22 19:35:52 +0200

    Challenge 2 Temperature Converter

---
+++ Challenge 2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Challenge_2
+{
+    internal class Program
+    {
+        //Temperature Converter: Convert Celsius to Fahrenheit and vice versa.
+        //Ex: 0°C => 32°F.
+        static void Main(string[] args)
+        {
+        double celsius = 25;
+        double fahrenheit = CelsiusToFahrenheit(celsius);
+        Console.WriteLine($"{celsius}°C is equal to {fahrenheit}°F");
+        }
+
+
+        public static double CelsiusToFahrenheit(double celsius) => ((celsius * 9 / 5) + 32);
+
+        public static double FahrenheitToCelsius(double fahrenheit) => ((fahrenheit - 32) * 5 / 9);
+    }
+}

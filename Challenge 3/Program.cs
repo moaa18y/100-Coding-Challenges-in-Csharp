2e54dc88635abe2a2dd88ea19cdbb7e6665fd406
Moamen Abu Al-Saud <moaa18y@gmail.com>
2026-04-22 19:45:36 +0200

    Challenge 3  Even/Odd Checker

---
+++ Challenge 3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Challenge_3
+{
+    internal class Program
+    {
+        //Even/Odd Checker: Check if a number is even or odd.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to check:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            CheckEvenOdd(number);
+        }
+
+        public static void CheckEvenOdd(int number)
+        {
+            if(number % 2 ==0)
+                Console.WriteLine($"Number: {number} is even");
+            else
+                Console.WriteLine($"Number: {number} is odd");
+        }
+
+    }
+}

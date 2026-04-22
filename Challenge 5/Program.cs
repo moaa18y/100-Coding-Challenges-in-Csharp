5f4e04c0e9d99c0ee4b65483b9dc2896b43fbd1c
Moamen Abu Al-Saud <moaa18y@gmail.com>
2026-04-22 23:09:18 +0200

    Challenge 5 String Reverser

---
+++ Challenge 5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Challenge_5
+{
+    internal class Program
+    {
+        //String Reverser: Reverse a given string without using built-in reverse methods.
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string to reverse: ");
+            string input = Console.ReadLine();
+            string reversed = ReverseString(input); 
+            Console.WriteLine($"Reversed string: {reversed}");
+        }
+        public static string ReverseString(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                throw new ArgumentException("Cannot be null.");
+            char[] reversedChar = new char[input.Length];
+            for (int i = 0; i < input.Length; i++)
+                reversedChar[i] = input[input.Length - 1 - i];
+            return new string(reversedChar);
+        }
+    }
+}

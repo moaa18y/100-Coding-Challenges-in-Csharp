bac0c1258a146b63b6baf0bc745c5bdedf1bb602
Moamen Abu Al-Saud <moaa18y@gmail.com>
2026-04-22 19:16:49 +0200

    Tax Calculator

--- Challenge 1/Program.cs
+++ Challenge 1/Program.cs
@@ -2,9 +2,27 @@
 {
     internal class Program
     {
+
+        //Tax Calculator: Write a method calculating net salary after tax.
+        // Ex: Calc(1000, 0.1) => 900
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double grossSalary = 1000;
+            double taxRate = 0.1;
+            double NetSalary=CalculateNetSalary(grossSalary,taxRate);
+            Console.WriteLine($"Net salary: {NetSalary:C} \nGross Salary: {grossSalary:C} \nTax Rate: {taxRate:P} ");
+
+        }
+
+        public static double CalculateNetSalary(double grossSalary, double taxRate)
+        {
+            if (grossSalary<0 || taxRate<0 || taxRate>1)
+            {
+                throw new ArgumentException("Gross Salary must be positive and TaxRate between 0 and 1.");
+            }
+            double taxAmount = grossSalary * taxRate;
+            return grossSalary - taxAmount;
+
         }
     }
 }

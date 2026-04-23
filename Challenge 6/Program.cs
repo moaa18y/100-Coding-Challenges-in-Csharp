e646415514e8b26be01e803a7652c01716db79e9
Moamen Abu Al-Saud <moaa18y@gmail.com>
2026-04-23 14:58:25 +0200

    Challenge 6 FizzBuzz App

---
+++ Challenge 6/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Challenge_6
+{
+    internal class Program
+    {
+        //FizzBuzz app
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---FizzBuzz---");
+            Console.Write("Please Enter Number of Iterations: ");
+
+            int.TryParse(Console.ReadLine(),out int count);
+
+            FizzBuzz(count);
+
+        }
+
+        public static int FizzBuzz(int count)
+        {
+            for (int i = 1; i <= count; i++)
+            {
+                if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else if (i % 7 == 0)
+                    Console.WriteLine("Lucky");
+                else if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("FizzBuzz");
+                else
+                    Console.WriteLine(i);
+            }
+            return count;
+        }
+    }
+}
